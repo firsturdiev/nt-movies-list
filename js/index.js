dda93518f44c43085ddda70f4fdd2890a436b484
Samandar Turdiev <firsturdiev@gmail.com>
2021-09-13 21:36:18 +0500

    rft

--- js/index.js
+++ js/index.js
@@ -3,24 +3,30 @@
 var elMoviesList = document.querySelector('.movies');
 var tempMovie = document.querySelector('#tempMovie').content;
 var moviesFragment = document.createDocumentFragment();
-var movies = moviesData.slice(0, 50);
+var movies = moviesData.slice(0, 500);
 
-for (var i = 0; i < movies.length; i++) {
-  var newMovie = tempMovie.cloneNode(true);
-  newMovie.querySelector('.movie__poster').src = `http://i3.ytimg.com/vi/${movies[i].ytid}/maxresdefault.jpg`;
-  newMovie.querySelector('.movie__poster').alt = `Poster of ${movies[i].Title}`;
-  newMovie.querySelector('.movie__title').textContent = movies[i].Title;
-  newMovie.querySelector('.movie__title').title = movies[i].Title;
-  newMovie.querySelector('.movie__info-rating').textContent = movies[i].imdb_rating;
-  newMovie.querySelector('.movie__info-year').textContent = movies[i].movie_year;
-  newMovie.querySelector('.movie__info-duration').textContent = `${Math.ceil(movies[i].runtime / 60)}h ${movies[i].runtime % 60}min`;
-  newMovie.querySelector('.movie__genres').textContent = movies[i].Categories.split('|').join(', ');
-  newMovie.querySelector('.movie__more-link').dataset.index = i;
+function showMovies(movies) {
+  elMoviesList.innerHTML = '';
 
-  moviesFragment.appendChild(newMovie);
+  for (var i = 0; i < movies.length; i++) {
+    var newMovie = tempMovie.cloneNode(true);
+    newMovie.querySelector('.movie__poster').src = movies[i].youtubePoster;
+    newMovie.querySelector('.movie__poster').alt = `Poster of ${movies[i].title}`;
+    newMovie.querySelector('.movie__title').textContent = movies[i].title;
+    newMovie.querySelector('.movie__title').title = movies[i].title;
+    newMovie.querySelector('.movie__info-rating').textContent = movies[i].imdbRating;
+    newMovie.querySelector('.movie__info-year').textContent = movies[i].year;
+    newMovie.querySelector('.movie__info-duration').textContent = `${Math.ceil(movies[i].runtime / 60)}h ${movies[i].runtime % 60}min`;
+    newMovie.querySelector('.movie__genres').textContent = movies[i].categories.join(', ');
+    newMovie.querySelector('.movie__more-link').dataset.index = i;
+
+    moviesFragment.appendChild(newMovie);
+  }
+
+  elMoviesList.appendChild(moviesFragment);
 }
 
-elMoviesList.appendChild(moviesFragment);
+showMovies(movies);
 
 
 // Show movie info 
@@ -31,12 +37,42 @@ var elMovieModal = document.querySelector('#movieModal');
 elMovieMoreBtn.forEach(btn => {
   btn.addEventListener('click', () => {
     var movie = movies[Number(btn.dataset.index)];
-    elMovieModal.querySelector('.movie__title').textContent = movie.Title;
-      elMovieModal.querySelector('.movie__info-rating').textContent = movie.imdb_rating;
-      elMovieModal.querySelector('.movie__info-year').textContent = movie.movie_year;
-      elMovieModal.querySelector('.movie__info-duration').textContent = `${Math.ceil(movie.runtime / 60)}h ${movie.runtime % 60}min`;
-      elMovieModal.querySelector('.movie__trailer').src = `https://www.youtube.com/embed/${movie.ytid}`;
-      elMovieModal.querySelector('.movie__summary').textContent = movie.summary;
-      elMovieModal.querySelector('.movie__imdb-link').href = `https://www.imdb.com/title/${movie.imdb_id}`;
+    elMovieModal.querySelector('.movie__title').textContent = movie.title;
+    elMovieModal.querySelector('.movie__info-rating').textContent = movie.imdbRating;
+    elMovieModal.querySelector('.movie__info-year').textContent = movie.year;
+    elMovieModal.querySelector('.movie__info-duration').textContent = `${Math.ceil(movie.runtime / 60)}h ${movie.runtime % 60}min`;
+    elMovieModal.querySelector('.movie__trailer').src = `https://www.youtube.com/embed/${movie.youtubeId}`;
+    elMovieModal.querySelector('.movie__summary').textContent = movie.summary;
+    elMovieModal.querySelector('.movie__imdb-link').href = `https://www.imdb.com/title/${movie.imdbId}`;
   })
+})
+
+// Filters
+
+let filteredMovies = [];
+// let categories = ['Uncategorized', 'Documentary', 'Music', 'Adventure', 'Animation', 'Comedy', 'Family', 'Fantasy', 'Horror', 'Drama', 'Sport', 'Romance', 'Action', 'Sci-Fi', 'News', 'History', 'Thriller', 'Western', 'Crime', 'Mystery', 'Biography', 'Musical', 'War', 'Reality-TV'];
+const elFilters = document.querySelector('.filters');
+const elFiltersQuery = elFilters.q;
+const elFiltersCategories = elFilters.categories;
+const elFiltersYearMin = elFilters.from_year;
+const elFiltersYearMax = elFilters.to_year;
+const elFiltersRating = elFilters.rating;
+
+elFilters.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let regexSearch = new RegExp(elFiltersQuery.value, 'gi');
+  filteredMovies = movies.filter(movie => {
+    return (
+      (movie.title.match(regexSearch)) &&
+      (elFiltersCategories.value.includes('all') || movie.categories.includes(elFiltersCategories.value)) &&
+      (movie.year >= (Number(elFiltersYearMin.value) || 1900) && movie.year <= (Number(elFiltersYearMax.value) || 2021)) &&
+      (movie.imdbRating >= Number(elFiltersRating.value))
+    );
+  });
+
+if (filteredMovies.length > 0)
+  showMovies(filteredMovies);
+else
+  elMoviesList.innerHTML = '<p class="text-center fw-bold h2 mb-0">No movie found</p>'
 })
