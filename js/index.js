ca3353617c21ba3bd4c6803fe8979b041db491db
Samandar Turdiev <firsturdiev@gmail.com>
2021-09-14 20:36:32 +0500

    Fix bu

--- js/index.js
+++ js/index.js
@@ -66,6 +66,13 @@ function filterMovies(arr, option) {
       return 0;
     })
   }
+  else if (option === 'z-a') {
+    arr.sort((a, b) => {
+      if (a.title > b.title) return -1;
+      if (a.title < b.title) return 1;
+      return 0;
+    })
+  }
   else if (option === 'rating') {
     arr.sort((a, b) => {
       return b.imdbRating - a.imdbRating;
